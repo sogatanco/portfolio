0b72fbf91c784d33e7dc118ee6f8b7c17f9024e6
sogatanco <adeliapucha46@gmail.com>
2019-07-14 01:32:07 +0700

    created navbar and jumbotron

--- application/views/home.php
+++ application/views/home.php
@@ -10,6 +10,36 @@
     <title>Sogata.Co | My PortFolio</title>
 </head>
 <body>
+
+<!-- start navbar -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <a class="navbar-brand" href="#">
+        <img src="<?php echo base_url();?>asset/img/mylogo.png" alt="Sogata.Co" width="200px">
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav ml-auto">
+          <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+          <a class="nav-item nav-link" href="#">About</a>
+          <a class="nav-item nav-link" href="#">Services</a>
+          <a class="nav-item nav-link" href="#">Portfolio</a>
+          <a class="nav-item nav-link" href="#">Contact</a>
+        </div>
+      </div>
+      </div>
+  </nav>
+<!-- end navbar -->
+
+<!-- home -->
+  <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-4">Fluid jumbotron</h1>
+      <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+  </div>
+<!-- end home -->
   
 </body>
 </html>
