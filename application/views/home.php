6dd47837c2905daf9fc4f350d5aa71ea6ceb730a
sogatanco <adeliapucha46@gmail.com>
2019-07-24 23:48:46 +0700

    added button on home

--- application/views/home.php
+++ application/views/home.php
@@ -1,9 +1,10 @@
 <div  id="home">
   <div class="jumbotron jumbotron-fluid">
     <div class="container">
-      <h1>Hi! I’m an <strong>Android</strong> and <strong>Web</strong> Developer.</h1>
-      <p>I master several programming languages ​​to develop web or android applications. 
-If you are a business need the services or an employer looking to hire, 
-you can get in touch with me.</p>
+        <h1>Hi! I’m an <strong>Android</strong> and <strong>Web</strong> Developer.</h1>
+        <p>I master several programming languages ​​to develop web or android applications. If you are a business need the services or an employer looking to hire, 
+you can get in touch with me.</p>   
+        <button class="btn btn-danger">Get in touch &nbsp;&nbsp;<i class="fas fa-angle-right"></i></button>
+        <button class="btn btn-light">View my CV &nbsp;&nbsp;<i class="fas fa-angle-right"></i></button>
   </div>
-  </div>
+</div>
