a44e0a12a311e8d3d2002ee4e1eb6526903a36d5
sogatanco <adeliapucha46@gmail.com>
2019-10-04 15:08:33 +0700

    update link

--- application/views/home.php
+++ application/views/home.php
@@ -5,7 +5,7 @@
         <p>I master several programming languages ​​to develop web or android applications. If you are a business need the services or an employer looking to hire, 
 you can get in touch with me.</p>   
         <a class="btn btn-danger">Get in touch &nbsp;&nbsp;<i class="fas fa-angle-right"></i></a>
-        <a href="http://sogata.co/asset/file/sogata_cv.pdf" class="btn btn-light">View my CV &nbsp;&nbsp;<i class="fas fa-angle-right"></i></a>
+        <a href="<?php echo base_url();?>asset/file/sogata_cv.pdf" class="btn btn-light">View my CV &nbsp;&nbsp;<i class="fas fa-angle-right"></i></a>
         <div class="scroll text-center">
           <img src="<?php echo base_url();?>asset/img/mouse.gif" >
           <br><small>SCROLL</small>
